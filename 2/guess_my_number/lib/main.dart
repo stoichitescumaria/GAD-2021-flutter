eface4821de1640b2fcc933bbd5baa1aa1745568
maria <stoichitescumaria@yahoo.com>
2021-07-04 15:26:34 +0300

    Added analysis_options.yaml

--- 2/guess_my_number/lib/main.dart
+++ 2/guess_my_number/lib/main.dart
@@ -6,42 +6,46 @@ import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _amount;
+  String? _amount;
   bool _reset = false;
   bool _popUp = false;
   int _number = -1;
-  String _message;
-  final _controler = TextEditingController();
+  String? _message;
+  final TextEditingController _controler = TextEditingController();
 
   void initNumber() {
     _message = null;
-    Random random = new Random();
+    final Random random = Random();
     _number = random.nextInt(100) + 1;
   }
 
   void generateMessage(int value) {
     if (value < _number) {
-      _message = "You tried " + value.toString() + "\nTry higher";
+      _message = 'You tried $value\nTry higher';
     } else if (value > _number) {
-      _message = "You tried " + value.toString() + "\nTry lower";
+      _message = 'You tried $value\nTry lower';
     } else {
       _reset = true;
       _popUp = true;
@@ -53,132 +57,144 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Guess my number"),
+        title: const Text('Guess my number'),
         centerTitle: true,
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const Text(
+            "I'm thinking of a number a number between 1 and 100.",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const Text(
+            "It's your turn to guess my number!",
+            style: TextStyle(
+              fontSize: 15,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          if (_message != null)
             Text(
-              "I'm thinking of a number a number between 1 and 100.",
-              style: TextStyle(
-                fontSize: 20,
+              _message!,
+              style: const TextStyle(
+                fontSize: 25,
               ),
               textAlign: TextAlign.center,
             ),
-            Text(
-              "It's your turn to guess my number!",
-              style: TextStyle(
-                fontSize: 15,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            if (_message != null)
-              Text(
-                _message,
-                style: TextStyle(
-                  fontSize: 25,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 50.0, horizontal: 24.0),
-              child: Container(
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 24.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: <BoxShadow>[
                   BoxShadow(
                     color: Colors.grey,
                     spreadRadius: 1,
                     blurRadius: 10,
                   )
-                ]),
-                child: Column(
-                  children: [
-                    Text(
-                      "Try a number",
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                      textAlign: TextAlign.center,
+                ],
+              ),
+              child: Column(
+                children: <Widget>[
+                  const Text(
+                    'Try a number',
+                    style: TextStyle(
+                      fontSize: 30,
                     ),
-                    TextField(
-                        controller: _controler,
-                        keyboardType: TextInputType.number,
-                        onChanged: (String s) {
-                          _amount = s;
-                        }),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: ElevatedButton(
-                          child: _reset ? Text("Reset") : Text("Guess"),
-                          onPressed: () {
-                            if (_reset = true) {
-                              setState(() {
-                                _reset = false;
-                                return;
-                              });
-                            }
-                            if (_amount != null) {
-                              try {
-                                int x = int.parse(_amount);
-                                setState(() {
-                                  if (_number == -1) {
-                                    initNumber();
-                                  }
-                                  generateMessage(x);
-                                });
-                              } on FormatException {
-                                setState(
-                                  () {},
-                                );
-                              }
-                            }
-                            if (_popUp == true) {
-                              return showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  title: Text("You guessed right"),
-                                  content: Text("It was " + _number.toString()),
-                                  actions: <Widget>[
-                                    ElevatedButton(
-                                        child: Text("Try again?"),
-                                        onPressed: () {
-                                          setState(() {
-                                            _reset = false;
-                                            _popUp = false;
-                                            _number = -1;
-                                            _controler.clear();
-                                            _amount = "";
-                                            Navigator.of(context).pop();
-                                          });
-                                        }),
-                                    ElevatedButton(
-                                        child: Text("OK"),
-                                        onPressed: () {
-                                          setState(() {
-                                            _reset = true;
-                                            _popUp = false;
-                                            _number = -1;
-                                            _amount = "";
-                                            _controler.clear();
-                                            Navigator.of(context).pop();
-                                          });
-                                        }),
-                                  ],
+                    textAlign: TextAlign.center,
+                  ),
+                  TextField(
+                    controller: _controler,
+                    keyboardType: TextInputType.number,
+                    onChanged: (String s) {
+                      _amount = s;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      child: _reset ? const Text('Reset') : const Text('Guess'),
+                      onPressed: () {
+                        if (_reset = true) {
+                          setState(
+                            () {
+                              _reset = false;
+                              return;
+                            },
+                          );
+                        }
+                        if (_amount != null) {
+                          try {
+                            final int x = int.parse(_amount!);
+                            setState(
+                              () {
+                                if (_number == -1) {
+                                  initNumber();
+                                }
+                                generateMessage(x);
+                              },
+                            );
+                          } on FormatException {
+                            setState(
+                              () {},
+                            );
+                          }
+                        }
+                        if (_popUp == true) {
+                          showDialog<void>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                              title: const Text('You guessed right'),
+                              content: Text('It was $_number'),
+                              actions: <Widget>[
+                                ElevatedButton(
+                                  child: const Text('Try again?'),
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        _reset = false;
+                                        _popUp = false;
+                                        _number = -1;
+                                        _controler.clear();
+                                        _amount = '';
+                                        Navigator.of(context).pop();
+                                      },
+                                    );
+                                  },
                                 ),
-                              );
-                            }
-                          }),
+                                ElevatedButton(
+                                  child: const Text('OK'),
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        _reset = true;
+                                        _popUp = false;
+                                        _number = -1;
+                                        _amount = '';
+                                        _controler.clear();
+                                        Navigator.of(context).pop();
+                                      },
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- 2/number_shape/lib/main.dart
+++ 2/number_shape/lib/main.dart
@@ -6,77 +6,81 @@ import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _amount;
-  final _controler = TextEditingController();
-  String printInfo() {
-      _controler.clear();
-      int number =  int.parse(_amount);
-      double root = sqrt(number);
-      bool square = false;
-      bool triangular = false;
-      if(root % 1 == 0) {
-        square = true;
-      }
-      root = pow(number, 1/3);
-      if(root % 1 == 0) {
-        triangular = true;
-      }
-      if(triangular && square) {
-        return "Number " + number.toString() + " is both SQUARE AND TRIANGULAR.";
-      } else if(square) {
-        return "Number " + number.toString() + " is SQUARE.";
-      } else if(triangular) {
-        return "Number " + number.toString() + " is TRIANGULAR.";
-      }
-      return "Number " + number.toString() + " is neither SQUARE AND TRIANGULAR.";
+  late String? _amount;
+  final TextEditingController _controller = TextEditingController();
 
+  String printInfo() {
+    _controller.clear();
+    final int number = int.parse(_amount!);
+    double root = sqrt(number);
+    bool square = false;
+    bool triangular = false;
+    if (root % 1 == 0) {
+      square = true;
+    }
+    root = pow(number, 1 / 3) as double;
+    if (root % 1 == 0) {
+      triangular = true;
+    }
+    if (triangular && square) {
+      return 'Number $number is both SQUARE AND TRIANGULAR.';
+    } else if (square) {
+      return 'Number $number is SQUARE.';
+    } else if (triangular) {
+      return 'Number $number is TRIANGULAR.';
+    }
+    return 'Number $number is neither SQUARE AND TRIANGULAR.';
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Number Shapes"),
+        title: const Text('Number Shapes'),
         centerTitle: true,
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Please input a number to see if it is square or triangular.",
-              style: TextStyle(
-                fontSize: 20,
-              ),
-              textAlign: TextAlign.center,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const Text(
+            'Please input a number to see if it is square or triangular.',
+            style: TextStyle(
+              fontSize: 20,
             ),
-            TextField(
-                controller: _controler,
-                keyboardType: TextInputType.number,
-                onChanged: (String s) {
-                  _amount = s;
-                }),
-          ],
-        ),
+            textAlign: TextAlign.center,
+          ),
+          TextField(
+            controller: _controller,
+            keyboardType: TextInputType.number,
+            onChanged: (String s) {
+              _amount = s;
+            },
+          ),
+        ],
       ),
       floatingActionButton: IconButton(
         disabledColor: Colors.black,
@@ -86,10 +90,10 @@ class _MyHomePageState extends State<MyHomePage> {
           color: Colors.blue,
         ),
         onPressed: () {
-          return showDialog(
+          showDialog<void>(
             context: context,
-            builder: (context) => AlertDialog(
-              title: Text(_amount),
+            builder: (BuildContext context) => AlertDialog(
+              title: Text(_amount!),
               content: Text(printInfo()),
             ),
           );
