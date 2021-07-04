66e86bc0d3e8c1fe17f5a0ac6392a152a5b9a3dc
maria <stoichitescumaria@yahoo.com>
2021-07-04 16:09:55 +0300

    Github actions

--- 2/guess_my_number/android/app/src/main/kotlin/com/example/guess_my_number/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.guess_my_number
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 2/number_shape/android/app/src/main/kotlin/com/example/number_shape/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.number_shape
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
